d3a08a51963f77b4a8cf7cdc73d4a6ba98d10366
cssullivan4 <cssullivan4@gmail.com>
2017-06-01 16:16:41 -0400

    improvements

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var myFriends = ['Heidi','Sean','Edi','Madeline','Zdravka'];
 
-for (var f = 0; f < myFriends.length; f++) {
-    console.log(myFriends[f] + ': ');
-    for (var n = 99; n > 1; n--) {
-       console.log([n] + ' lines of code in the file, ' + [n] + ' lines of code; ' + myFriends[f] + ' strikes one out, clears it all out, ' + [n] + ' lines of code in the file!');
-    } if (n = 1) {
-       console.log('1 line of code in the file, 1 line of code; ' + myFriends[f] + ' strikes one out, clears it all out, no more lines of code in the file!');
-    } else
-       console.log('No more lines of code in the file? No more lines of code! Strike a key, indent a statement, we need more lines of code in the file!');
-   }
+for (var i = 0; i < myFriends.length; i++) {
+    console.log(myFriends[i].toUpperCase() + ': ');
+    for (var j = 99; j > 0; j--) {
+        if (j > 2) {
+            console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + myFriends[i] + ' strikes one out, clears it all out, ' + (j-1) + ' lines of code in the file!');
+        } else if (j === 2) {
+            console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + myFriends[i] + ' strikes one out, clears it all out, ' + (j-1) + ' line of code in the file!');
+        } else {
+            console.log('1 line of code in the file, 1 line of code; ' + myFriends[i] + ' strikes one out, clears it all out, no more lines of code in the file!');
+        }
+    }
+};
